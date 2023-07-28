bca20ceeb3cfb0403d4774790e86a85754e582c1
chernandez83 <104525460+chernandez83@users.noreply.github.com>
2023-07-28 17:32:58 -0600

    Primera carga

---
+++ lib/Pages/MyHomePage.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:my_first_app/Widgets/MyText.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: Colors.deepOrangeAccent[100],
+       body: SafeArea(
+         child: MyText(),
+       ),
+    );
+  }
+}
---
+++ lib/Widgets/MyText.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MyText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return const Text(
+      "¡Hola Flutter! Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 30,
+        fontWeight: FontWeight.normal,
+        letterSpacing: 2,
+        wordSpacing: 5,
+        backgroundColor: Colors.deepOrangeAccent,
+        fontFamily: "REM",
+        decoration: TextDecoration.underline,
+        decorationColor: Colors.white,
+      ),
+      textAlign: TextAlign.justify,
+      softWrap: true,
+      maxLines: 2,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+
+}
